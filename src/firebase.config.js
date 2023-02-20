6cf8e5c01f82c439a68d8330f2b8988e6616ae35
nathanMulugeta01 <nathanmulugetacoding@gmail.com>
2023-02-20 10:45:28 +0300

    finished with the pages and routing

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { getFireStore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBBkTJ-tC5WFsOpq1LN0YsUVPBKg8ci58",
+  authDomain: "house-marketplace-app-12f45.firebaseapp.com",
+  projectId: "house-marketplace-app-12f45",
+  storageBucket: "house-marketplace-app-12f45.appspot.com",
+  messagingSenderId: "8422490787",
+  appId: "1:8422490787:web:992aac5374cbf74976f516",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
+function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = () => {};
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+        <main>
+          <form>
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="passwordInput"
+                placeholder="Password"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+              <img src={visibilityIcon} alt="" className="showPassword" />
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default SignIn;
